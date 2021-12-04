23cc612531253de01b3ac6d9ee69fd4c6a6cfcb1
MahdiBehi <78915698+MahdiBehiEsprit@users.noreply.github.com>
2021-12-04 12:10:20 +0100

    new responsive navbar

---
+++ src/app/responsive-toolbar/responsive-toolbar.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../menu-item';
+
+@Component({
+  selector: 'app-responsive-toolbar',
+  templateUrl: './responsive-toolbar.component.html',
+  styleUrls: ['./responsive-toolbar.component.css']
+})
+export class ResponsiveToolbarComponent implements OnInit {
+
+  menuItems: MenuItem[] = [
+    {
+      label: 'Sign Up',
+      icon: 'login',
+      route: 'login',
+      showOnMobile: true,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'About',
+      icon: 'help',
+      route: 'about',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'Invoices',
+      icon: 'attach_money',
+      route: 'facture',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'Products',
+      icon: 'shop',
+      route: 'products',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'Showcase',
+      icon: 'slideshow',
+      route: '',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: false
+    },
+    {
+      label: 'Blog',
+      icon: 'rss_feed',
+      route: '',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: false
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
