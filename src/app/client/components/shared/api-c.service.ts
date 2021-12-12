da03df38a1a3d183e8209d5983ef6495884cbec1
MahdiBehi <78915698+MahdiBehiEsprit@users.noreply.github.com>
2021-12-12 10:35:46 +0100

    client integrated

--- src/app/client/components/shared/api-c.service.ts
+++ src/app/client/components/shared/api-c.service.ts
@@ -12,38 +12,37 @@ export class ApiCService {
 
   retrieveClientById(idClient: number): Observable<Client>
   {
-    return this.http.get<Client>("http://localhost:8095/SpringMVC/client/fetch/" + idClient);
+    return this.http.get<Client>("http://localhost:8089/SpringMVC/client/fetch/" + idClient);
   }
 
   retrieveAllClients(): Observable<Client[]>
   {
-    return this.http.get<Client[]>("http://localhost:8095/SpringMVC/client/all");
+    return this.http.get<Client[]>("http://localhost:8089/SpringMVC/client/all");
   }
 
   addClient(client: Client): Observable<Client>
   {
-    return this.http.post<Client>("http://localhost:8095/SpringMVC/client/add/", client);
+    return this.http.post<Client>("http://localhost:8089/SpringMVC/client/add/", client);
   }
 
   updateClient(client: Client): Observable<Client>
   {
-    return this.http.put<Client>("http://localhost:8095/SpringMVC/client/update/", client);
+    return this.http.put<Client>("http://localhost:8089/SpringMVC/client/update/", client);
   }
 
   updateClientById(idClient: number, client: Client): Observable<Client>
   {
-    return this.http.put<Client>("http://localhost:8095/SpringMVC/client/update/" + idClient, client);
-  
+    return this.http.put<Client>("http://localhost:8089/SpringMVC/client/update/" + idClient, client);
   }
 
   deleteClient(idClient: number): Observable<void>
   {
-    return this.http.delete<void>("http://localhost:8095/SpringMVC/client/delete/"+ idClient);
+    return this.http.delete<void>("http://localhost:8089/SpringMVC/client/delete/"+ idClient);
   }
 
   clientProfessionStats(prof:string): Observable<any>
   {
-    return this.http.get<number>("http://localhost:8095/clientProfessionPourcentage/"+prof);
+    return this.http.get<number>("http://localhost:8089/clientProfessionPourcentage/"+prof);
   }
   
 }
